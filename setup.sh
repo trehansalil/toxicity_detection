3bc0e8953112b23036ef8be540c68b0b1e4e60d3
Salil Trehan <trehansalil1@gmail.com>
2024-04-28 06:15:02 +0000

    Some more updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
 sudo apt -y install python3.10-venv
 python3 -m venv env
 sudo apt -y install python3-pip
+source env/bin/activate
 pip install -r requirements.txt
